e03f7669bbc03607887de906ddf257564d08836b
Victor Petit <contact@petitvictor.fr>
2021-11-21 13:36:52 +0100

    Add Help Reward commands

--- src/main/java/devarea/bot/event/ReactionAdd.java
+++ src/main/java/devarea/bot/event/ReactionAdd.java
@@ -1,9 +1,6 @@
 package devarea.bot.event;
 
-import devarea.bot.automatical.FreeLanceManager;
-import devarea.bot.automatical.MeetupManager;
-import devarea.bot.automatical.MissionsManager;
-import devarea.bot.automatical.RolesReacts;
+import devarea.bot.automatical.*;
 import devarea.bot.commands.CommandManager;
 import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.object.entity.Message;
@@ -14,13 +11,13 @@ public class ReactionAdd {
 
     public static void reactionAddFunction(ReactionAddEvent event) {
         try {
-            if (!event.getMember().isPresent()) {
+            if (event.getMember().isEmpty()) {
                 final Message message = event.getMessage().block();
                 message.getChannel().block().createMessage(messageCreateSpec -> messageCreateSpec.setContent(messageDisableInPrivate)).subscribe();
                 return;
             }
 
-            if (event.getMember().get().isBot() || RolesReacts.onReact(event))
+            if (event.getMember().get().isBot() || RolesReacts.onReact(event) || HelpRewardManager.react(event))
                 return;
 
             if (CommandManager.react(event) || MissionsManager.react(event) || FreeLanceManager.react(event) || MeetupManager.getEvent(event))
