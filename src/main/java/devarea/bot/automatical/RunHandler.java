b568f670d5c9cfe0c00a42f14c937cc0b2cc4b76
mathias4833 <anseaumemathias@gmail.com>
2022-07-16 22:54:15 +0200

    Update //run output based on user message edit
    When the user edits a recent //run command that has already been executed, it is reexecuted and the original bot's response is updated.

---
+++ src/main/java/devarea/bot/automatical/RunHandler.java
@@ -0,0 +1,68 @@
+package devarea.bot.automatical;
+
+import devarea.bot.Init;
+import devarea.bot.commands.inLine.Run;
+import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.TextChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import discord4j.core.spec.MessageEditSpec;
+import discord4j.rest.util.AllowedMentions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RunHandler {
+
+    final static int MAX_MESSAGES = 400;
+
+    private static final Map<Snowflake, Snowflake> latestMessages = new LinkedHashMap<>();
+
+    public static boolean onEdit(Message message) {
+        if (!latestMessages.containsKey(message.getId())
+                || !message.getContent().startsWith(Init.initial.prefix + "run")
+                || message.getAuthor().isEmpty()) {
+            return false;
+        }
+
+        TextChannel channel = (TextChannel) message.getChannel().block();
+
+        if (channel == null) {
+            return false;
+        }
+
+        new Run(message.getAuthor().get().asMember(Init.devarea.getId()).block(), channel, message);
+        return true;
+    }
+
+    public static void addMessage(Snowflake message, Snowflake reply) {
+        latestMessages.put(message, reply);
+        if (latestMessages.size() > MAX_MESSAGES) {
+            latestMessages.remove(latestMessages.keySet().iterator().next());
+        }
+    }
+
+    public static void sendResponse(Message message, EmbedCreateSpec spec, boolean edit) {
+        Snowflake replyId = latestMessages.get(message.getId());
+        TextChannel channel = (TextChannel) message.getChannel().block();
+        Message reply;
+
+        if (channel == null) {
+            return;
+        }
+
+        if (edit && replyId != null && (reply = channel.getMessageById(replyId).block()) != null) {
+            reply.edit(MessageEditSpec.builder().addEmbed(spec).build()).subscribe();
+        } else if (replyId == null) {
+            reply = channel.createMessage(MessageCreateSpec.builder()
+                    .addEmbed(spec)
+                    .messageReference(message.getId())
+                    .allowedMentions(AllowedMentions.suppressAll())
+                    .build()).block();
+            if (edit && reply != null) {
+                addMessage(message.getId(), reply.getId());
+            }
+        }
+    }
+}
